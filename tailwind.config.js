466af7248f05ab28843403a27cdea451d0875ac0
roxiTheReal <aetheron.inc@proton.me>
2025-05-30 21:34:58 +0200

    Created website

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Inter', sans-serif"]
+      },
+      shadow: {
+        
+      }
+    },
+  },
+  plugins: [],
+}
